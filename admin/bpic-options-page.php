4d32d0b706478ace6b1aae4a70b4752f5fe63fd3
Richard Phelps <richard.phelps123@hotmail.com>
2016-06-05 16:49:39 +0100

    IC

---
+++ admin/bpic-options-page.php
@@ -0,0 +1,20 @@
+<?php
+    /**
+     *
+     * Admin area for BuddyPress Instant Chat plugin
+     *
+     */
+
+    if (!defined('ABSPATH')) exit; // Exit if accessed directly
+
+    if (!current_user_can('manage_options')) {
+        wp_die( __('Unfortunately you must be an admin to make changes on this page', 'bpic') );
+    }
+?>
+<div class="wrap">
+    <h2><?php _e('BuddyPress Instant Chat', 'bpic'); ?></h2>
+
+    <form method="post" action="options.php">
+        <?php //settings_fields(); ?>
+    </form>
+</div>
